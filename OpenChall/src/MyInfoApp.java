21884e2f7d1d1261ec5b7e41946b239e657025b6
user <user@Note-3-24-2>
2024-09-09 19:49:03 +0900

    ffff

---
+++ OpenChall/src/MyInfoApp.java
@@ -0,0 +1,12 @@
+
+public class MyInfoApp {
+	public static void main(String[] args) {
+		int age = 20;
+		System.out.println("Kitae Hwang");
+		System.out.println(age + " years old");
+		System.out.println("Department of Computer Engineering");
+		System.out.println("Kitae Hwang");
+		System.out.println("Kitae Hwaaang");
+		System.out.println("Kitae Hwang");
+	}
+}
